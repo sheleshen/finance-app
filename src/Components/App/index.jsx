a65bf3b5390196c0d4563df595f4ea6ec10d2afa
sheleshen <sheleshnewaanna@gmail.com>
2023-11-21 13:45:14 +0300

    Компонент Balance

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -1,10 +1,12 @@
-import 'components/App/style.css';
-import Header from 'components/Header';
+import "components/App/style.css";
+import Header from "components/Header";
+import Balance from "components/Balance";
 
 function App() {
   return (
-    <div>
+    <div className="max-w-7xl m-auto">
       <Header />
+      <Balance />
     </div>
   );
 }
--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,21 +1,18 @@
-import 'components/Header/style.css'
+import "components/Header/style.css";
 
 const Header = () => {
-    return (
-        <div className='container'>
-            <div className='container py-8 flex justify-between items-center'>
-                <h1 className='text-5xl text-black font-extrabold'>
-                    Привет, Аня
-                </h1>
-                <div className='w-14 h-14 bg-stone-300 rounded-full'></div>
-            </div>
-            <div className='grid grid-cols-2 gap-10 border-t-4 border-stone-300'>
-                <p className='font-bold text-4xl text-center pt-3'>Расходы</p>
-                <p className='font-bold text-4xl text-center pt-3'>Доходы</p>
-                
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="">
+      <div className="py-8 flex justify-between items-center">
+        <h1 className="text-5xl text-black font-extrabold">Привет, Аня</h1>
+        <div className="w-14 h-14 bg-stone-300 rounded-full"></div>
+      </div>
+      <div className="grid grid-cols-2 gap-10 border-t-4 border-stone-300">
+        <p className="font-bold text-4xl text-center pt-3">Расходы</p>
+        <p className="font-bold text-4xl text-center pt-3">Доходы</p>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
