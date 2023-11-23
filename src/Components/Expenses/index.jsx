7a71af90f5e889a6748d9298436d46f54096a53e
sheleshen <sheleshnewaanna@gmail.com>
2023-11-24 00:33:46 +0300

    Коррекции

--- src/Components/Expenses/index.jsx
+++ src/Components/Expenses/index.jsx
@@ -1,22 +1,16 @@
 import 'components/Expenses/style.css'
 import Button from 'components/Button'
 
-const categories = ['Путешествия', 'Развлечения', 'Образование', 'Еда', 'Одежда', 'Ипотека']
+const categories = ['Путешествия', 'Развлечения', 'Образование', 'Еда', 'Одежда', 'Ипотека', 'ЖКХ', 'Здоровье', 'Связь', 'Транспорт', 'Косметика']
 
 const Expenses = () => {
      return (
         <div className='bg-expenses mt-16 rounded-t-60'>
             <div className='max-w-7xl m-auto'>
                 <div className='flex gap-4 justify-center py-16'>
-                    <input name="amount" type="number" placeholder='00,00' className='px-5 py-2 border border-solid rounded-full stroke-field text-slate-900 stroke-outline'/>
-                    {/* <input name="category" type="text" className='px-5 py-2 border border-solid rounded-full stroke-field'/>  */}
-                    <select name="categories" className='px-5 py-2 border border-solid rounded-full stroke-field text-slate-900 stroke-outline'> 
-                        {categories.map(category => {
-                            return (
-                                <option>{category}</option>
-                            )
-                        })}
-                        
+                    <input name="amount" type="number" placeholder='00,00' className='px-6 py-3 text-lg border border-solid rounded-full stroke-field text-slate-900 stroke-outline'/>
+                    <select name="categories" className='px-6 py-3 text-lg border border-solid rounded-full stroke-field text-slate-900 stroke-outline'> 
+                        {categories.map(category => <option key={category}>{category}</option>)}
                     </select>
                     <Button title='Добавить' />
                 </div>
--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -3,10 +3,10 @@ import "components/Header/style.css";
 const Header = () => {
   return (
     <div className="max-w-7xl m-auto">
-      <div className="py-8 flex justify-between items-center">
+      {/* <div className="py-8 flex justify-between items-center">
         <h1 className="text-5xl text-black font-extrabold">Привет, Аня</h1>
         <div className="w-14 h-14 bg-stone-300 rounded-full"></div>
-      </div>
+      </div> */}
       <div className="grid grid-cols-2 gap-10 border-t-4 border-stone-300">
         <p className="font-bold text-4xl text-center pt-3">Расходы</p>
         <p className="font-bold text-4xl text-center pt-3">Доходы</p>
