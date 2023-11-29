2bd62c0babd4a97189b1bbde5ff8477ffa57fb61
sheleshen <sheleshnewaanna@gmail.com>
2023-11-29 16:11:28 +0300

    Форматирование

--- src/Components/Expenses/index.jsx
+++ src/Components/Expenses/index.jsx
@@ -1,25 +1,29 @@
-import 'components/Expenses/style.css'
-import ExpensesForm from 'components/ExpensesForm'
+import "components/Expenses/style.css";
+import ExpensesForm from "components/ExpensesForm";
 
 const Expenses = (/*props*/) => {
-    // const { newExpense } = props;
+  // const { newExpense } = props;
 
-    const currentDate = new Date().toLocaleDateString();
+  const currentDate = new Date().toLocaleDateString();
 
-    return (
-        <div className='bg-expenses mt-16 rounded-t-60'>
-            <ExpensesForm />
-            <div className='max-w-7xl m-auto pb-16'>
-                <div className='flex justify-between'>
-                    <div className='flex gap-5 items-center'>
-                        <p className=" text-gray-500 font-semibold text-base">{currentDate}</p>
-                        <p className="text-3xl font-bold">{/*newExpense.category*/}Категория</p>
-                    </div>
-                    <p className="text-3xl">- {/*newExpense.spending*/}2 999 ₽</p>
-                </div>
-            </div>    
+  return (
+    <div className="bg-expenses mt-16 rounded-t-60">
+      <ExpensesForm />
+      <div className="max-w-7xl m-auto pb-16">
+        <div className="flex justify-between">
+          <div className="flex gap-5 items-center">
+            <p className=" text-gray-500 font-semibold text-base">
+              {currentDate}
+            </p>
+            <p className="text-3xl font-bold">
+              {/*newExpense.category*/}Категория
+            </p>
+          </div>
+          <p className="text-3xl">- {/*newExpense.spending*/}2 999 ₽</p>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Expenses
+export default Expenses;
--- src/Components/ExpensesForm/index.jsx
+++ src/Components/ExpensesForm/index.jsx
@@ -19,48 +19,45 @@ const unsortedCategories = [
 const categories = unsortedCategories.sort();
 
 const ExpensesForm = () => {
-    const [ spending, setSpending ] = useState("")
-    const [ category, setCategory ] = useState(categories[0]);
+  const [spending, setSpending] = useState("");
+  const [category, setCategory] = useState(categories[0]);
 
-    const handleClick = () => {
-        const newExpens = {
-            spending,
-            category,
-        };
+  const handleClick = () => {
+    const newExpens = {
+      spending,
+      category,
+    };
 
-        // Надо записать новые данные const newExpens в область расходов, пока не знаю как
+    // Надо записать новые данные const newExpens в область расходов, пока не знаю как, но их видно в консоли
 
-        console.log(newExpens)
-        setSpending("");
-        setCategory(categories[0]);
-    }
+    console.log(newExpens);
+    setSpending("");
+    setCategory(categories[0]);
+  };
 
-    return (
-        <div className="flex gap-4 justify-center py-16">
-        <input
-            onChange={
-                (event) => setSpending(event.target.value)
-            }
-            value={spending}
-            name="spending"
-            type="number"
-            min="0"
-            placeholder="00,00"
-            className="px-6 py-3 text-lg border border-solid rounded-full stroke-field text-slate-900 stroke-outline"
-        />
-        <select
-            onChange={
-                (event) => setCategory(event.target.value)
-            }
-            value={category}
-            className="px-6 py-3 text-lg border border-solid rounded-full stroke-field text-slate-900 stroke-outline"
-        >
-            {categories.map((category) => (
-            <option key={category}>{category}</option>
-            ))}
-        </select>
-        {<Button title="Добавить" handleClick={handleClick} type="submit" />}
-        </div>
+  return (
+    <div className="flex gap-4 justify-center py-16">
+      <input
+        onChange={(event) => setSpending(event.target.value)}
+        value={spending}
+        name="spending"
+        type="number"
+        min="0"
+        placeholder="00,00"
+        className="px-6 py-3 text-lg border border-solid rounded-full stroke-field text-slate-900 stroke-outline"
+      />
+      <select
+        onChange={(event) => setCategory(event.target.value)}
+        value={category}
+        name="category"
+        className="px-6 py-3 text-lg border border-solid rounded-full stroke-field text-slate-900 stroke-outline"
+      >
+        {categories.map((category) => (
+          <option key={category}>{category}</option>
+        ))}
+      </select>
+      {<Button title="Добавить" handleClick={handleClick} type="submit" />}
+    </div>
   );
 };
 
