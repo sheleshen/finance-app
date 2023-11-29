6dbca0d116b8d3894415378a3eaf31c0382e336e
sheleshen <sheleshnewaanna@gmail.com>
2023-11-29 14:44:38 +0300

    Компонент Expenses

--- src/Components/Expenses/index.jsx
+++ src/Components/Expenses/index.jsx
@@ -1,22 +1,22 @@
 import 'components/Expenses/style.css'
-import Button from 'components/Button'
-
-const categories = ['Путешествия', 'Развлечения', 'Образование', 'Еда', 'Одежда', 'Ипотека', 'ЖКХ', 'Здоровье', 'Связь', 'Транспорт', 'Косметика']
+// import ExpensesForm from 'components/ExpensesForm'
 
 const Expenses = () => {
-     return (
+    const currentDate = new Date().toLocaleDateString();
+
+    return (
         <div className='bg-expenses mt-16 rounded-t-60'>
             <div className='max-w-7xl m-auto'>
-                <div className='flex gap-4 justify-center py-16'>
-                    <input name="amount" type="number" placeholder='00,00' className='px-6 py-3 text-lg border border-solid rounded-full stroke-field text-slate-900 stroke-outline'/>
-                    <select name="categories" className='px-6 py-3 text-lg border border-solid rounded-full stroke-field text-slate-900 stroke-outline'> 
-                        {categories.map(category => <option key={category}>{category}</option>)}
-                    </select>
-                    <Button title='Добавить' />
+                <div className='flex justify-between'>
+                    <div className='flex gap-5 items-center'>
+                        <p className=" text-gray-500 font-bold text-lg">{currentDate}</p>
+                        <p className="text-3xl font-bold">Категория</p>
+                    </div>
+                    <p className="text-3xl">- 2 000 ₽</p>
                 </div>
             </div>    
         </div>
-     )
+    )
 }
 
 export default Expenses
--- src/Components/ExpensesForm/index.jsx
+++ src/Components/ExpensesForm/index.jsx
@@ -8,16 +8,12 @@ const ExpensesForm = () => {
     // const [ categories, setCategories ] = useState();
 
      return (
-        <div className='bg-expenses mt-16 rounded-t-60'>
-            <div className='max-w-7xl m-auto'>
-                <div className='flex gap-4 justify-center py-16'>
-                    <input name="amount" type="number" placeholder='00,00' className='px-6 py-3 text-lg border border-solid rounded-full stroke-field text-slate-900 stroke-outline'/>
-                    <select name="categories" className='px-6 py-3 text-lg border border-solid rounded-full stroke-field text-slate-900 stroke-outline'> 
-                        {categories.map(category => <option key={category}>{category}</option>)}
-                    </select>
-                    {<Button title='Добавить' /* handleClick={handleClick}*/ type="submit" /> }
-                </div>
-            </div>    
+        <div className='flex gap-4 justify-center py-16'>
+            <input name="amount" type="number" placeholder='00,00' className='px-6 py-3 text-lg border border-solid rounded-full stroke-field text-slate-900 stroke-outline'/>
+            <select name="categories" className='px-6 py-3 text-lg border border-solid rounded-full stroke-field text-slate-900 stroke-outline'> 
+                {categories.map(category => <option key={category}>{category}</option>)}
+            </select>
+            {<Button title='Добавить' /* handleClick={handleClick}*/ type="submit" /> }
         </div>
      )
 }
